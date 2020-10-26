af0ca41308935a30f77915c35b9972b78e9d6aff
Anonoz Chong <hello@anonoz.com>
2020-10-26 21:44:46 +0800

    Update build-production.sh

--- build-production.sh
+++ build-production.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 TAG=$(git rev-parse HEAD)
-docker build -t anonoz/fyp-webapp-arm64:$TAG -f Dockerfile.prod .
+docker build -t anonoz/fyp-webapp-arm64:$TAG -f Dockerfile .
 docker tag anonoz/fyp-webapp-arm64:$TAG anonoz/fyp-webapp-arm64:latest
 docker push anonoz/fyp-webapp-arm64:$TAG
 docker push anonoz/fyp-webapp-arm64:latest
